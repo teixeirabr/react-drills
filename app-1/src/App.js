f0613e830ecc9d7b59539229bf32f9ac1cceb155
andre <andrelaprov@gmail.com>
2020-02-28 22:49:10 -0700

    app-1 finished

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      message: ""
+    };
+  }
+
+  handleChange(e) {
+    console.log(e.target.value);
+    this.setState({
+      message: e.target.value
+    });
+  }
+
+  render() {
+    console.log(this.state.message);
+    return (
+      <div className="App">
+        <p>App-1</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Create a basic react app where you type in a text box and it shows up
+          as text on the DOM.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <input type="text" onChange={e => this.handleChange(e)} />
+        <p>{this.state.message}</p>
+      </div>
+    );
+  }
 }
-
 export default App;
